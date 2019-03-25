809ebc57dad78fcf02a6c976c8d40ca4e2f8c936
bruicejonson <48694694+bruicejonson@users.noreply.github.com>
2019-03-25 15:54:31 -0400

    Other Assignments

---
+++ class.js
@@ -0,0 +1,16 @@
+var  myvar = 1
+switch(myvar){
+
+case 1: myvar = 1;{
+console.log("Hey");
+break
+
+}case 2: myvar = 2;{
+console.log("Foo","Bar");
+break
+
+}case 3: myvcar =3;{
+console.log("Bar");
+break
+}
+}
